f1bac815e0cec1932da178c258139d7b830d7dfd
aleizn <119465229+aleizn@users.noreply.github.com>
2023-05-06 21:31:47 -0500

    Aleizn

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -8,8 +8,8 @@ let oi = `*《 𝐌𝐞𝐧𝐬𝐚𝐣𝐞 》:* ${pesan}`
 let teks = `*Bot creado Wa.me/51992621601 no permito la reventa
 😼《 𝐈𝐍𝐕𝐎𝐂𝐀𝐍𝐃𝐎 - 𝐆𝐑𝐔𝐏𝐎 》*\n\n❏ ${oi}\n\n❏ *《 ❁𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚𝐬 》:*\n`
 for (let mem of participants) {
-teks += `✼❤️‍🔥 @${mem.id.split('@')[0]}\n`}
-teks += `➥𝐀𝐥𝐞𝐢𝐳𝐧-𝐌𝐃`
+teks += `✼🐼 @${mem.id.split('@')[0]}\n`}
+teks += `➥𝐉𝐞𝐧𝐢 -𝐈𝐯𝐥𝐞𝐲`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
